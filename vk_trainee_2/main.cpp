25d05476003a91f7ac6dd08f9207bb05c95a54a7
Fedor <novikovfedor@gmail.com>
2019-05-28 21:17:35 +0300

    approximate uniq counter

--- vk_trainee_2/main.cpp
+++ vk_trainee_2/main.cpp
@@ -1,17 +1,66 @@
-#include <set>
+﻿#include <set>
 #include <random>
 #include <cassert>
+#include <unordered_set>
+#include <iostream>
+#include <numeric>
+#include <cmath>
+
+using namespace std;
 
 class UniqCounter {
-	// no more than 32kb of memory should be used here
+private:	
+	const int section_count_ = 32768 / sizeof(int);
+	int max_section_size_ = 1;
+
+	// no more than 32kb of memory should be used here + sizeof(std::vector<int>)
+	// we will store only approximate sum of different numbers on section
+	vector<int> sections_ = vector<int>(section_count_, 0);
+	random_device rd_;
+	mt19937 gen_ = mt19937(rd_());
+	uniform_int_distribution<> dis_ = uniform_int_distribution<int>(1, max_section_size_);
+
+	// from start each section contains only one number, but than we have to reduce accuracy
+	void change_section_size_(int max_x)
+	{
+		const int old_section_size = max_section_size_;
+		max_section_size_ = static_cast<int>(pow(2, log2(max_x) - log2(section_count_) + 1));
+		vector<int> sections_old = sections_;
+		sections_ = vector<int>(section_count_, 0);
+
+		const size_t section_grow = max_section_size_ / old_section_size;
+		for (size_t i = 0; i < section_count_ / section_grow; i++)
+		{
+			int sum = 0;
+			for (size_t j = 0; j < section_grow; j++)
+			{
+				sum += sections_old[i*section_grow + j];
+			}
+			sections_[i] = sum;
+		}
+	
+		dis_ = uniform_int_distribution<int>(1, max_section_size_);
+	}
 
 public:
+	// it can be assumed x >= 0
 	void add(int x) {
-		// add some code
+		if (x > max_section_size_ * section_count_)
+		{
+			change_section_size_(x);
+		}
+
+		const int current_section_size = sections_[x / max_section_size_];
+		const int rand = dis_(gen_);
+		if (rand > current_section_size)
+		{
+			sections_[x / max_section_size_]++;
+		}
 	}
 
 	int get_uniq_num() const {
-		return 0;
+		const int count = accumulate(sections_.begin(), sections_.end(), 0);
+		return count;
 	}
 };
 
